bf4680d2721a6988dbece65838e995f3c3557535
aweffr <aweffr@qq.com>
2017-05-08 17:21:26 +0800

    init

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from random import shuffle, randint
+from json import dumps
+# http://blog.csdn.net/u014030117/article/details/46508901
+# Flask对Jinja2模版引擎支持很好，但无奈其所有静态文件都要放在static文件夹中（URL路由得加/static/...）
+# 实例化Flask类的时候做一个小设置static_url_path=''即可，
+# 把static_url_path设置为空字符串相当于设置把所有根目录下URL的访问都关联到/static/目录下，
+# 所以静态HTML模版中直接可以引用/js/something.js而不是/static/js/something.js
+app = Flask(__name__, static_url_path='')
+
+xx = [x for x in range(100)]
+yy = [75, ]
+for i in range(99):
+    yy.append(yy[-1] + randint(-3, 4))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pages/<page_name>')
+def pages(page_name):
+    return render_template('./pages/%s' % page_name)
+
+@app.route('/getdata')
+def getdata():
+    yy = [85,]
+    for i in range(99):
+        yy.append(yy[-1] + randint(-2, 5))
+    d = {'xx':[x for x in range(100)],
+         'yy':yy}
+    return dumps(d)
+
+@app.route('/updatedata')
+def update_data():
+    global xx, yy
+    xx.append(xx[-1] + 1)
+    yy.append(yy[-1] + randint(-9, 15))
+    d = {'x':xx[-1], 'y': yy[-1]}
+    return dumps(d)
+
+
+if __name__ == '__main__':
+    app.run(port=12000)
---
+++ static/data/uav-data.js
@@ -0,0 +1,79 @@
+/**
+ * Created by ZouLe on 2017/5/5.
+ */
+var myChart = null;
+var xx = [];
+var yy = [];
+var option = null;
+
+function plot(data) {
+    xx = data['xx'];
+    yy = data['yy'];
+
+    // $("#debug").text(" " + xx + " " + yy);
+
+    option = {
+        title: {
+            text: '臭氧——时间',
+            left: 'left'
+        },
+        tooltip: {},
+        legend: {
+            data: ['臭氧']
+        },
+        xAxis: {
+            data: xx
+        },
+        yAxis: {},
+        tooltip: {
+            trigger: 'axis'
+        },
+        series: [{
+            name: '臭氧',
+            type: 'line',
+            data: yy,
+            smooth: true
+        }]
+    };
+
+    myChart = echarts.init(document.getElementById('uav-line-chart'));
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+}
+
+function update_data(data) {
+    xx.push(data['x']);
+    xx.shift();
+    yy.push(data['y']);
+    yy.shift();
+}
+
+function update_chart() {
+    myChart.setOption({
+        xAxis: {
+            data: xx
+        },
+        series: [{
+            name: '臭氧',
+            type: 'line',
+            data: yy
+        }]
+    })
+}
+
+$(document).ready(function () {
+    $.ajax({
+        url: "/getdata",
+        dataType: "json",
+        success: plot
+    });
+    setInterval(function () {
+        $.ajax({
+            url: "/updatedata",
+            dataType: "json",
+            success: update_data
+        })
+    }, 333);
+    setInterval(update_chart, 1000);
+});
