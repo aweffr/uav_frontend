48e8f8619e63e4aed9f096c406ec5b2140880bf7
aweffr <aweffr@qq.com>
2017-06-24 09:18:29 +0800

    12321

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 
 app = Flask(__name__)
 
-xx = [(datetime.datetime.now() + datetime.timedelta(seconds=(x-60))).strftime("%H:%M:%S") for x in range(60)]
+xx = [(datetime.datetime.now() + datetime.timedelta(seconds=(x - 60))).strftime("%H:%M:%S") for x in range(60)]
 yy = [0, ]
 for i in range(59):
     yy.append(yy[-1] + 0.5 * randint(0, 0))
@@ -54,7 +54,16 @@ def update_data():
 def get_ozone_history_data():
     # Test stage
     ozone_xx = [time.strftime(datetime.datetime(2017, 5, i).strftime("%Y-%m-%d")) for i in range(6, 14)]
-    ozone_yy = [52 + 8 * (random() - 0.5) for i in range(len(ozone_xx))]
+    # ozone_yy = [52 + 8 * (random() - 0.5) for i in range(len(ozone_xx))]
+    ozone_yy = [54.13354220083586,
+                54.40904295827208,
+                52.837885234874825,
+                50.57564263353102,
+                48.07532339032751,
+                49.794336520820465,
+                49.63183682389971,
+                49.46190356155412]
+
     d = {
         "xx": ozone_xx,
         "yy": ozone_yy
@@ -67,7 +76,15 @@ def get_ozone_history_data():
 def get_pm25_history_data():
     # Test stage
     pm25_xx = [time.strftime(datetime.datetime(2017, 5, i).strftime("%Y-%m-%d")) for i in range(6, 14)]
-    pm25_yy = [78 + 40 * (random() - 0.5) for i in range(len(pm25_xx))]
+    pm25_yy = [72.31210130392026,
+               74.36003202003266,
+               81.0815528593375,
+               85.44115429140395,
+               80.97432689427235,
+               77.09845096931669,
+               69.45107713653275,
+               65.97297080702435]
+
     d = {
         "xx": pm25_xx,
         "yy": pm25_yy
